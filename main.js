0802cbce2f344163c21b69041b2114533c1eb582
javierbarbozam <barboza.javier497@gmail.com>
2023-02-17 18:03:05 -0600

    Homework finished

--- main.js
+++ main.js
@@ -1,35 +1,58 @@
-// Escriba una function que escriba un número a la inversa
+// Crear un método que permita agregar cursos a la lista de cursos aprobados.
+// El usuario debe poder ingresar un objeto con el nombre del curso, nota.
 
-function numeroReversa(value) {
-  number = value.toString();
-  newNumber = "";
+// Y dependiendo la nota se agrega de manera automática una propiedad nueva que se llama estado:
+// cuyo valor sera de aprobado (nota > 70) o reprobado (nota < 70)
 
-  for (let i = number.length - 1; i >= 0; i--) {
-    newNumber += number[i];
+function estudianteNuevo(nombre, apellidos, fechaNacimiento, id, nuevoIngreso, carrera, matriculados, cursados) {
+  const datos = {};
+
+  datos.nombre = nombre;
+  datos.apellidos = apellidos;
+  datos.fechaNacimiento = fechaNacimiento;
+  datos.id = id;
+  datos.nuevoIngreso = nuevoIngreso;
+  datos.carrera = carrera;
+  datos.matriculados = matriculados || [];
+  datos.cursados = cursados || [];
+  
+  datos.matricular = function(curso) {
+    this.matriculados.push(curso)
   }
 
-  return Number(newNumber);
-}
-
-console.log(numeroReversa(123));
-
-
-// Escriba una function que acepte un string como parámetro y convierta cada
-// primer letra de cada palabra en mayúscula
-
-
-function toUpperCase(phrase) {
-  let value = phrase.split(" ");
-  for (i = 0; i < value.length; i++) {
-    value[i] = value[i].split("");
-    let firstLetter = value[i].shift();
-    value[i].unshift(firstLetter.toUpperCase());
-    value[i] = value[i].join("");
+  datos.retirar = function(curso) {
+    const index = this.matriculados.indexOf(curso);
+    if (index > -1) {
+      this.matriculados.splice(index, 1);
+    }
+  
+    // this.cursosMatriculados = this.cursosMatriculados.filter(item => item !== curso); // devuelve un array con los elementos que cumplen con la condición
   }
 
-  phrase = value.join(" ");
+  datos.materias = function (curso, nota) {
+    const materia = {};
 
-  return phrase;
+    materia.curso = curso;
+    materia.nota = nota;
+    materia.estado = '';
+
+    if (nota >= 70) {
+      materia.estado = 'aprobado'
+    } else {materia.estado = 'reprobado'}
+
+    // materia.estado = function (nota) {
+    //   if (nota >= 70) {
+    //     return 'aprobado'
+    //   } else { return 'reprobado'}
+    // }
+
+    this.cursados.push(materia)
+  }
+
+  return datos;
 }
 
-console.log(toUpperCase("hola esto es una prueba"));
+const andy = estudianteNuevo('Andy', 'Smith', '1990-10-30', 'a-0001', true, 'Historía del Arte', ['Introducción a la pintura']);
+andy.materias('Introducción a la pintura', 70)
+andy.materias('Humanidades I', 68)
+console.log(andy)
