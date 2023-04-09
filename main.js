70147d25e6a5f0b8e160d1b1aeb6b6da3244dd27
javierbarbozam <barboza.javier497@gmail.com>
2023-04-09 00:28:49 -0600

    Add fetch code for every API

--- main.js
+++ main.js
@@ -1,13 +1,44 @@
-function parImpar (number) {
-  return new Promise (function(resolve, reject){
-    number%2===0?resolve(`${number} es par`):reject(`${number} es impar`)
-  })
+// Nombre, 
+// imagen, 
+// altura, 
+// peso,
+// habilidades (solo nombre de habilidades)
+// stats: Nombre y base_stat
+
+const pokemonSelect = document.getElementById('pokemonSelect');
+const pokemonDetail = document.getElementById('pokemon-detail');
+
+// fetching all Pokemon from API
+const pokemonApiData = async () => {
+  const response = await fetch('https://pokeapi.co/api/v2/pokemon?offset=110&limit=100');
+  const data = await response.json()
+  pokemonList(data.results)
+}
+pokemonApiData()
+
+// Creates all the <option> HTML tags
+const pokemonList = (item) => {
+  let list = '<option>Seleccione un Pokémon</option>';
+  item.forEach(element => {
+    list += `<option value="${element.url.slice(34,37)}">${element.name}</option>` //slice in order to have specific id from url
+  });
+  pokemonSelect.innerHTML = list
 }
 
-parImpar(3)
-  .then(res => {
-    console.log(res)
+function selectPokemonListener () {
+  pokemonSelect.addEventListener ('change', () => {
+    let pokemonId = pokemonSelect.value
+    loadPokemon(pokemonId)
   })
-  .catch(error => {
-    console.log(error)
-  })
+}
+selectPokemonListener()
+
+const loadPokemon = async (pokemonId) => {
+  if (pokemonId != 'Seleccione un Pokémon') {
+    const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonId}`);
+    const data = await response.json()
+    console.log(data)
+  }
+}
+
+const pokemonInformation = () => {}
