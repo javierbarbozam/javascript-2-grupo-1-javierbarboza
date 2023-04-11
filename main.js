bc1eb68116914e758399fa1d859316a4e7292191
javierbarbozam <barboza.javier497@gmail.com>
2023-04-11 02:20:48 -0600

    Change info form spanish to english

--- main.js
+++ main.js
@@ -11,7 +11,7 @@ pokemonApiData()
 
 // Creates all the <option> HTML tags
 const pokemonList = (data) => {
-  let list = '<option>Seleccione un Pokémon</option>';
+  let list = '<option>Choose a Pokémon</option>';
   data.forEach(element => {
     list += `<option value="${element.url.slice(34,37)}">${dataGrammar(element.name)}</option>` //slice in order to have specific id from url
   });
@@ -29,7 +29,7 @@ pokemonSelectListener()
 
 // Specific Pokemon API function
 const loadPokemon = async (pokemonId) => {
-  if (pokemonId != 'Seleccione un Pokémon') {
+  if (pokemonId != 'Choose a Pokémon') {
     const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonId}`);
     const data = await response.json()
     pokemonInformation(data)
@@ -43,21 +43,21 @@ const pokemonInformation = (data) => {
     <h2 class="pokemon-name">${dataGrammar(data.name)}</h2>
     <div class="info-grid">
       <div>
-        <h3 class="subtitle">Altura</h3>
+        <h3 class="subtitle">Height</h3>
         <p>${data.height}</p>
       </div>
       <div>
-        <h3 class="subtitle">Peso</h3>
+        <h3 class="subtitle">Weight</h3>
         <p>${data.weight}</p>
       </div>
       <div>
-        <h3 class="subtitle">Habilidades</h3>
+        <h3 class="subtitle">Abilities</h3>
         <ul class="pokemon-abilities">
           ${data.abilities.map((item) => {return `<li>${dataGrammar(item.ability.name)}</li>`}).join('')}
         </ul>
       </div>
       <div>
-        <h3 class="subtitle">Estadísticas</h3>
+        <h3 class="subtitle">Stats</h3>
         <ul class="pokemon-stats">
           ${data.stats.map((item) => {
             return `
